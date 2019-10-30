9d66aefd65039eea6ffcbe28535e11e1b52c2195
CarZaGo <57167859+CarZaGo@users.noreply.github.com>
2019-10-30 02:43:32 -0500

    Update and rename .gitignore to update.py

---
+++ update.py
@@ -0,0 +1,32 @@
+#!/bin/env python2
+#code owner = "CarZaGo"
+#
+import urllib2
+import os
+
+ver = open("version.txt", "r")
+version = ver.read()
+ver.close()
+
+up = urllib2.urlopen("https://raw.githubusercontent.com/CarZaGo/czg/master/version.txt").read()
+if version != up:
+	print ""
+	print "[+] Update available"
+	print ""
+	x = raw_input("Want to update y/n: ")
+	if x == "y":
+		os.remove("jdash.py")
+		CarZaGo = "https://raw.githubusercontent.com/CarZaGo/czg/master/czg.py"
+		update = urllib2.urlopen(CarZaGo).read()
+		
+		jdash = open("jdash.py", "w")
+		jdash.write(update)
+		jdash.close()
+
+		os.remove("version.txt")
+		ver = open("version.txt", "w")
+		ver.write(up)
+else:
+	print ""
+	print "[-] No update available right now"
+	print ""
